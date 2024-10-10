e120e1d69b2fca2d97794e1eb717bb4b875d92ee
root <root@Reem.localdomain>
2024-10-10 17:31:42 +0300

    floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to compute the floor of a float.
+"""
+
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Calculate the floor of a float.
+
+    Args:
+        n (float): The number to calculate the floor of.
+
+    Returns:
+        int: The floor of the given number.
+    """
+    return math.floor(n)
