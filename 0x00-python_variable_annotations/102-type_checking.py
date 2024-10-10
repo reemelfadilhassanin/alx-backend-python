fa079d071a871776e47e3be82104a339e724e777
root <root@Reem.localdomain>
2024-10-10 18:12:56 +0300

    type checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,31 +1,23 @@
 #!/usr/bin/env python3
 """
-This module to zoom in on an array
+This module provides a function to zoom in on an array
 """
+from typing import Union, Any, Mapping, Tuple, List
 
-from typing import List
 
-
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
-    Zoom into a list by a specified factor.
-
-    Args:
-        lst (List[int]): The list of integers to zoom in on.
-        factor (int, optional): The factor by which to zoom. Defaults to 2.
-
-    Returns:
-        List[int]: A new list containing the zoomed-in elements.
+    zoom
     """
-    zoomed_in: List[int] = [
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = tuple([12, 72, 91])
 
 zoom_2x = zoom_array(array)
 
-zoom_3x = zoom_array(array, 3)
+zoom_3x = zoom_array(array, int(3.0))
