422dcab5c2067b855fbd76b7ebc7db8073a5e4a5
root <root@Reem.localdomain>
2024-10-10 17:34:37 +0300

    convert to stringfunction

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module provides a function to compute the floor of a float.
+This module to compute the floor of a float.
 """
 
 import math
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module to convert a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Convert a float to a string.
+
+    Args:
+        n (float): The number to convert.
+
+    Returns:
+        str: The string representation of the float.
+    """
+    return str(n)
