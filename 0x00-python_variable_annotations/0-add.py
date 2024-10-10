d016597a9d2fbd114a3f3acc0ec1a129fac9d165
root <root@Reem.localdomain>
2024-10-10 17:15:16 +0300

    add uing annotation python

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to add two floating point numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    Add two floating point numbers.
+
+    Args:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
