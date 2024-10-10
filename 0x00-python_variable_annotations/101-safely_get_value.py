c478486d41dcc3629432c5f94cbb0fdbcb2bd93d
root <root@Reem.localdomain>
2024-10-10 18:06:43 +0300

    function to get val from dic

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,30 +1,15 @@
 #!/usr/bin/env python3
 """
-This module to safely get a val from a dic.
+More involved type annotations
 """
-
-from typing import Mapping, Any, TypeVar, Union
-
+from typing import Mapping, Any, Union, TypeVar
 T = TypeVar('T')
 
 
-def safely_get_value(
-    dct: Mapping[Any, T],
-    key: Any,
-    default: Union[T, None] = None
-) -> Union[T, None]:
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
     """
-    Safely get a value from a dictionary.
-
-    Args:
-        dct (Mapping[Any, T]): The dictionary to search.
-        key (Any): The key to look for in the dictionary.
-        default (Union[T, None], optional): The value to return if the key
-        is not found. Defaults to None.
-
-    Returns:
-        Union[T, None]: The value associated with the key if it exists,
-        otherwise the default value.
+    safely get value
     """
     if key in dct:
         return dct[key]
