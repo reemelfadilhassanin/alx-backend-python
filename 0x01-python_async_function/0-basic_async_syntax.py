4ed4df942e4f6377222f5659dba0df2f0e0778ab
root <root@Reem.localdomain>
2024-10-14 20:55:35 +0300

    basic async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import asyncio
+import random
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Waits for a random delay between 0 and max_delay (inclusive)
+    and eventually returns the delay.
+
+    Args:
+        max_delay (int): The maximum delay in seconds. Defaults to 10.
+
+    Returns:
+        float: The actual delay time waited.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
