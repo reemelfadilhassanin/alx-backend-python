6c639a5b24f6f5ff3a8bf04bcc4db06452024697
root <root@Reem.localdomain>
2024-10-14 21:26:43 +0300

    tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -5,7 +5,7 @@ This module alters wait_n into a new function task_wait_n.
 
 import asyncio
 from typing import List
-from 3_tasks import task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
