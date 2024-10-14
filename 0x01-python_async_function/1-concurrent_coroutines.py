af000b0a48930b594a92550fc39839d33e800c74
root <root@Reem.localdomain>
2024-10-14 21:10:16 +0300

    concurent

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Spawns wait_random n times with the specified max_delay,
+    and returns a list of all the delays in ascending order.
+
+    Args:
+        n (int): The number of times to call wait_random.
+        max_delay (int): The maximum delay in seconds.
+
+    Returns:
+        List[float]: A list of the delays sorted in ascending order.
+    """
+    futures = [wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
+    return delays
