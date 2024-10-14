46cc12b0598a38e926975789cc289467541fa7fe
root <root@Reem.localdomain>
2024-10-14 21:23:55 +0300

    tasks

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -18,15 +18,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Returns:
         List[float]: A list of the delays sorted in ascending order.
     """
-    delays = []
-    tasks = []
-
-    for _ in range(n):
-        task = task_wait_random(max_delay)
-        tasks.append(task)
-
-    for task in asyncio.as_completed(tasks):
-        delay = await task
-        delays.append(delay)
-
+    futures = [task_wait_random(max_delay) for _ in range(n)]
+    futures = asyncio.as_completed(futures)
+    delays = [await future for future in futures]
     return delays
