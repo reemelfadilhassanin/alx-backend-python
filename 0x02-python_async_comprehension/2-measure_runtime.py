b83224e126923d9120178111991eb965b64f3f91
root <root@Reem.localdomain>
2024-10-15 19:42:47 +0300

    task2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""This module measures the runtime
+of async_comprehension executed in parallel."""
+
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Measures the runtime of executing
+    async_comprehension four times in parallel.
+
+    This coroutine uses asyncio.gather to
+    run async_comprehension four times concurrently
+    and returns the total runtime.
+    """
+    start_time = time.time()  # Record the start time
+
+    await asyncio.gather(
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension()
+    )
+
+    return time.time() - start_time  # Calculate and return total runtime
