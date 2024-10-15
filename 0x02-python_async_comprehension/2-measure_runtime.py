c5b1830af151acee4b094021c971179cb3c226fa
root <root@Reem.localdomain>
2024-10-15 19:49:59 +0300

    task2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -15,7 +15,7 @@ async def measure_runtime() -> float:
     run async_comprehension four times concurrently
     and returns the total runtime.
     """
-    start_time = time.time()  # Record the start time
+    start_time = time.perf_counter()  # Record the start time
 
     await asyncio.gather(
         async_comprehension(),
@@ -24,4 +24,4 @@ async def measure_runtime() -> float:
         async_comprehension()
     )
 
-    return time.time() - start_time  # Calculate and return total runtime
+    return time.perf_counter() - start_time
