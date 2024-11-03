cf1a4de86fad16d8badbe5f22dffb391c62817b1
root <root@Reem.localdomain>
2024-11-03 21:21:30 +0300

    add test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
-
+"""A module for testing the utils module.
+"""
 import unittest
+from typing import Dict, Tuple, Union
 from parameterized import parameterized
-from utils import access_nested_map  # Import the function you need to test
+from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Tests the `access_nested_map` function."""
+    
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map, path, expected):
-        """Test access_nested_map returns expected results."""
+    def test_access_nested_map(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
 if __name__ == '__main__':
