7b630e18473a9594e03a6d7e19eaff0ea5d03ed7
root <root@Reem.localdomain>
2024-11-03 21:32:36 +0300

    add test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -7,9 +7,10 @@ from parameterized import parameterized
 from unittest.mock import patch, Mock
 from utils import access_nested_map, get_json, memoize
 
+
 class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
-    
+
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -38,6 +39,7 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
+
 class TestGetJson(unittest.TestCase):
     """Tests the `get_json` function."""
 
@@ -49,17 +51,19 @@ class TestGetJson(unittest.TestCase):
         """Tests `get_json`'s output."""
         mock_response = Mock()
         mock_response.json.return_value = test_payload
-        
-        with patch('utils.requests.get', return_value=mock_response) as mock_get:
+
+        with patch('utils.requests.get',
+                   return_value=mock_response) as mock_get:
             self.assertEqual(get_json(test_url), test_payload)
             mock_get.assert_called_once_with(test_url)
 
+
 class TestMemoize(unittest.TestCase):
     """Tests the `memoize` decorator."""
-    
+
     def test_memoize(self) -> None:
         """Tests `memoize` functionality."""
-        
+
         class TestClass:
             def a_method(self) -> int:
                 return 42
@@ -68,15 +72,16 @@ class TestMemoize(unittest.TestCase):
             def a_property(self) -> int:
                 return self.a_method()
 
-        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+        with patch.object(TestClass, 'a_method',
+                          return_value=42) as mock_method:
             test_instance = TestClass()
-            
-            # Call the property twice
-            self.assertEqual(test_instance.a_property, 42)  # Use property access
-            self.assertEqual(test_instance.a_property, 42)  # Use property access again
 
+            # Call the property twice
+            self.assertEqual(test_instance.a_property, 42)
+            self.assertEqual(test_instance.a_property, 42)
             # Ensure a_method was called only once
             mock_method.assert_called_once()
+
 
 if __name__ == '__main__':
     unittest.main()
