6cf466d433731cdb24e87b3111979048bdced8be
root <root@Reem.localdomain>
2024-11-03 21:23:17 +0300

    add test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A module for testing the utils module.
+"""this module for testing the utils
 """
 import unittest
 from typing import Dict, Tuple, Union
@@ -22,6 +22,20 @@ class TestAccessNestedMap(unittest.TestCase):
             ) -> None:
         """Tests `access_nested_map`'s output."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            ) -> None:
+        """Tests `access_nested_map` raises KeyError."""
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+        self.assertEqual(str(context.exception), f"'{path[-1]}'")
 
 if __name__ == '__main__':
     unittest.main()
