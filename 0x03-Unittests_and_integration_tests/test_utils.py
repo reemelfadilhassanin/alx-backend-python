0dffc5cbc01647f93eebb7aa4faa10fd63d4e383
root <root@Reem.localdomain>
2024-11-03 21:24:40 +0300

    add test_utils

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""this module for testing the utils
+"""A module for testing the utils module.
 """
 import unittest
 from typing import Dict, Tuple, Union
 from parameterized import parameterized
-from utils import access_nested_map
+from unittest.mock import patch, Mock
+from utils import access_nested_map, get_json
 
 class TestAccessNestedMap(unittest.TestCase):
     """Tests the `access_nested_map` function."""
@@ -36,6 +37,33 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
         self.assertEqual(str(context.exception), f"'{path[-1]}'")
+
+class TestGetJson(unittest.TestCase):
+    """Tests the `get_json` function."""
+    
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url: str, test_payload: Dict) -> None:
+        """Tests `get_json`'s output."""
+        # Create a mock response object with a json method
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        
+        # Patch requests.get to return the mock response
+        requests_get = patch('utils.requests.get', return_value=mock_response)
+        requests_get.start()
+        
+        try:
+            # Call the function being tested
+            self.assertEqual(get_json(test_url), test_payload)
+            # Verify that requests.get was called once with test_url
+            mock_response.json.assert_called_once()
+            requests_get.assert_called_once_with(test_url)
+        finally:
+            requests_get.stop()
 
 if __name__ == '__main__':
     unittest.main()
