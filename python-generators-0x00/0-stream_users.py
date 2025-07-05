e14cee31f7d9104e34746eb6390852c0efad0578
reemelfadilhassanin <justreemjust49@gmail.com>
2025-07-05 07:28:16 +0200

    seed

---
+++ python-generators-0x00/0-stream_users.py
@@ -0,0 +1,10 @@
+import seed
+
+def stream_users():
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data")
+    for row in cursor:
+        yield row
+    cursor.close()
+    connection.close()
