33738a8a70a2cc44247ba947e5bb39aef6acda3f
reemelfadilhassanin <justreemjust49@gmail.com>
2025-07-05 07:35:27 +0200

    batch processing

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import seed
+
+def paginate_users(page_size, offset):
+    connection = seed.connect_to_prodev()
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (page_size, offset))
+    rows = cursor.fetchall()
+    connection.close()
+    return rows
+
+def lazy_paginate(page_size):
+    offset = 0
+    while True:
+        page = paginate_users(page_size, offset)
+        if not page:
+            break
+        yield page
+        offset += page_size
