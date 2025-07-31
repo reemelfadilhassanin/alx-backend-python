d14da96c50e0f1a287bc64b41d8689c18ffe816c
reemelfadilhassanin <justreemjust49@gmail.com>
2025-07-31 05:27:26 +0200

    models

--- Django-signals_orm-0x04/django_chat/messaging/tests.py
+++ Django-signals_orm-0x04/django_chat/messaging/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from .models import Message, MessageHistory
 
-# Create your tests here.
+User = get_user_model()
+
+class MessageHistoryTest(TestCase):
+    def test_edit_history_created(self):
+        u = User.objects.create(username='test')
+        msg = Message.objects.create(sender=u, recipient=u, content='hi')
+        msg.content = 'hello'
+        msg.save()
+        self.assertTrue(MessageHistory.objects.filter(message=msg).exists())
+        mh = MessageHistory.objects.get(message=msg)
+        self.assertEqual(mh.old_content, 'hi')
