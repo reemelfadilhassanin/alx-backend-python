4c80ad0154fe71ed6889f9a3cc1aac5a83fe16a8
reemelfadilhassanin <justreemjust49@gmail.com>
2025-08-02 15:22:06 +0200

    add users

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+# messaging/views.py
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib import messages
+
+@login_required
+def delete_user(request):
+    """Deletes the currently logged-in user account"""
+    user = request.user
+    user.delete()
+    messages.success(request, "Your account has been deleted.")
+    return redirect('account_deleted')  # This URL name should exist or be created
