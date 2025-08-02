0572927adb96279cd13b6418dcd8811b41a9daee
reemelfadilhassanin <justreemjust49@gmail.com>
2025-08-02 15:23:39 +0200

    add users

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -1,13 +1,36 @@
-# messaging/views.py
-
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
-from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
+from messaging.models import Message
+from django.db.models import Prefetch
+
+# Recursive helper to get all nested replies
+def get_all_replies(message):
+    replies = []
+    children = message.replies.all()
+    for reply in children:
+        replies.append(reply)
+        replies += get_all_replies(reply)
+    return replies
 
 @login_required
-def delete_user(request):
-    """Deletes the currently logged-in user account"""
-    user = request.user
-    user.delete()
-    messages.success(request, "Your account has been deleted.")
-    return redirect('account_deleted')  # This URL name should exist or be created
+def threaded_conversation_view(request, conversation_user_id):
+    from django.contrib.auth.models import User
+    other_user = get_object_or_404(User, id=conversation_user_id)
+
+    # Fetch top-level messages in the conversation
+    messages = Message.objects.filter(
+        sender=request.user, receiver=other_user, parent_message__isnull=True
+    ).select_related('sender', 'receiver').prefetch_related(
+        Prefetch('replies', queryset=Message.objects.select_related('sender'))
+    )
+
+    # Build thread trees
+    threads = []
+    for msg in messages:
+        thread = {
+            'message': msg,
+            'replies': get_all_replies(msg)
+        }
+        threads.append(thread)
+
+    return render(request, 'threaded_conversation.html', {'threads': threads})
