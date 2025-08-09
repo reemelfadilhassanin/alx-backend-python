58a31df95ce6a06a24be234801666133ec66304d
root <root@Reem>
2025-08-09 17:53:16 +0200

    docker

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -149,3 +149,16 @@ INSTALLED_APPS = [
     'chats',
     # ...
 ]
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.getenv('MYSQL_DATABASE'),
+        'USER': os.getenv('MYSQL_USER'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD'),
+        'HOST': os.getenv('MYSQL_HOST', 'db'),
+        'PORT': os.getenv('MYSQL_PORT', '3306'),
+    }
+}
+
