d2f0a23a780fea2be62b888eb5dea2e23319e0ec
root <root@Reem>
2025-08-10 14:25:27 +0200

    multible container

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -161,4 +161,16 @@ DATABASES = {
         'PORT': os.getenv('MYSQL_PORT', '3306'),
     }
 }
+import os
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQL_DB'),
+        'USER': os.environ.get('MYSQL_USER'),
+        'PASSWORD': os.environ.get('MYSQL_PASSWORD'),
+        'HOST': os.environ.get('MYSQL_HOST', 'db'),
+        'PORT': os.environ.get('MYSQL_PORT', '3306'),
+    }
+}
 
